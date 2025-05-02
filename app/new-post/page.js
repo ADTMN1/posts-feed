600cdad6d9838c1e9dd8c708b11a611651d92928
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-05-02 03:08:04 -0700

    separate server action file

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,43 +1,9 @@
+'use client'
 
+import { createPost } from "@/action/posts"
 import PostForm from "@/components/post-form"
-import { storePost } from "@/lib/posts"
-import { redirect } from "next/navigation"
 
 export default function NewPostPage() {
-    async function createPost(prevState, formData) {
 
-        "use server"
-
-        const title = formData.get("title")
-        const image = formData.get("image")
-        const content = formData.get("content")
-
-        await storePost({
-            imageUrl: '',
-            title,
-            content,
-            userId: 1
-        })
-        let errors = [];
-
-        if (!title || title.trim().length === 0) {
-            errors.push('Title is required.');
-        }
-
-        if (!content || content.trim().length === 0) {
-            errors.push('Content is required.');
-        }
-
-        if (!image || image.size === 0) {
-            errors.push('Image is required.');
-        }
-
-        if (errors.length > 0) {
-            return { errors };
-        }
-
-        redirect('/feed')
-
-    }
     return <PostForm action={createPost} />
 }
